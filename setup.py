eacd94e496f775258f07e6b4261b9eb23bcd0a43
Emily Zhou <zhouxemily@gmail.com>
2020-09-19 19:04:28 -0700

    Add files from ros-cordial-interaction repository

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+import os
+
+package_dirs = {
+    'interaction_engine': os.path.join('src', 'interaction-engine', 'interaction_engine'),
+}
+
+d = generate_distutils_setup(
+    packages=package_dirs.keys(),
+    package_dir=package_dirs,
+)
+setup(**d)
