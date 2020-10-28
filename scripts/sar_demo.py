ac7bcbc2be6284419cc6060ef868290f06eb85de
Emily Zhou <zhouxemily@gmail.com>
2020-10-28 15:06:00 -0700

    Use CordialInterface from interaction engine

--- scripts/sar_demo.py
+++ scripts/sar_demo.py
@@ -5,6 +5,7 @@ import os
 import random
 import rospy
 
+from interaction_engine.cordial_interface import CordialInterface
 from interaction_engine.database import Database
 from interaction_engine.int_engine import InteractionEngine
 from interaction_engine.interface import Interface
@@ -16,90 +17,6 @@ from cordial_msgs.msg import AskOnGuiAction, AskOnGuiGoal, MouseEvent
 from std_msgs.msg import Bool
 
 logging.basicConfig(level=logging.INFO)
-
-
-class CordialInterface(Interface):
-
-    def __init__(self, action_name, seconds_until_timeout=None):
-
-        super(CordialInterface, self).__init__(
-            output_function=None,
-            input_function=self.call_ask_action_service
-        )
-
-        self._action_name = action_name
-        self._cordial_action_client = actionlib.SimpleActionClient(self._action_name, AskOnGuiAction)
-        self._is_begin_interaction = False
-
-        self._screen_tap_listener = rospy.Subscriber(
-            "cordial/gui/event/mouse",
-            MouseEvent,
-            callback=self._screen_tap_listener_callback,
-            queue_size=1
-        )
-
-        if seconds_until_timeout is not None:
-            if type(seconds_until_timeout) is not int or not float:
-                raise TypeError("Timeout must be int or float")
-            if seconds_until_timeout <= 0:
-                raise ValueError("Timeout must be greater than 0")
-        self._seconds_until_timeout = seconds_until_timeout
-
-        rospy.init_node('cordial_client')
-
-        rospy.on_shutdown(self._cancel_goal)
-
-    def _format_user_input(self, message, user_input):
-        if message.message_type in [Message.Type.MULTIPLE_CHOICE, Message.Type.MULTIPLE_CHOICE_ONE_COLUMN]:
-            user_input = message.options.index(user_input)
-        return user_input
-
-    def call_ask_action_service(self, message, data_dict):
-        rospy.loginfo("Calling action")
-
-        if type(message) is not Message:
-            raise TypeError("Invalid message class.")
-
-        updated_message_content = self.update_message(message, data_dict)
-
-        goal = AskOnGuiGoal()
-        goal.type = message.message_type
-        goal.content = updated_message_content
-        goal.options = message.options
-        goal.args = message.args
-
-        self._cordial_action_client.wait_for_server()
-
-        self._cordial_action_client.send_goal(goal, feedback_cb=self._cordial_feedback_cb)
-        self._cordial_action_client.wait_for_result()
-        response = self._cordial_action_client.get_result()
-
-        if response.data is "":
-            response.data = message.default_input
-
-        return self._format_user_input(message, response.data)
-
-    def _cordial_feedback_cb(self, feedback):
-        if self._seconds_until_timeout is not None:
-            if feedback.time_passed > self._seconds_until_timeout:
-                print("Time passed: {}; goal cancelled".format(feedback.time_passed))
-                self._cordial_action_client.cancel_goal()
-
-    def _cancel_goal(self):
-        if self._cordial_action_client.get_state() == actionlib.SimpleGoalState.ACTIVE:
-            self._cordial_action_client.cancel_goal()
-
-    def _screen_tap_listener_callback(self, _):
-        self._is_begin_interaction = True
-
-    @property
-    def is_begin_interaction(self):
-        return self._is_begin_interaction
-
-    @is_begin_interaction.setter
-    def is_begin_interaction(self, is_begin_interaction):
-        self._is_begin_interaction = is_begin_interaction
-
 
 jokes = {
     "science": [
@@ -199,20 +116,12 @@ ask_to_chat = State(
     }
 )
 
-# whats_your_name = State(
-#     name=WHATS_YOUR_NAME,
-#     message_type=Message.Type.TEXT_ENTRY,
-#     content=["<emphasis level=\"strong\"> <prosody pitch=\"high\"> *happy* Hi!</prosody></emphasis> I'm <prosody "
-#              "pitch=\"medium\">Q</prosody><break strength=\"weak\"/>T<break strength=\"weak\"/> robot. "
-#              "*question_2*What's <emphasis level=\"strong\"><prosody rate=\"slow\">your</prosody></emphasis> name?"],
-#     next_states=[HOW_ARE_YOU],
-#     transitions={"Next": HOW_ARE_YOU},
-# )
 whats_your_name = State(
     name=WHATS_YOUR_NAME,
     message_type=Message.Type.TEXT_ENTRY,
-    content="What is a robot's favorite snack? *QT/yawn*<break time=\"0.75s\"/><prosody volume=\"loud\" rate=\"slow\">"
-        "*happy*Computer</prosody> <prosody rate=\"slow\" pitch=\"high\">chips</prosody>. Chomp, chomp!",
+    content=["<emphasis level=\"strong\"> <prosody pitch=\"high\"> *happy* Hi!</prosody></emphasis> I'm <prosody "
+             "pitch=\"medium\">Q</prosody><break strength=\"weak\"/>T<break strength=\"weak\"/> robot. "
+             "*question_2*What's <emphasis level=\"strong\"><prosody rate=\"slow\">your</prosody></emphasis> name?"],
     next_states=[HOW_ARE_YOU],
     transitions={"Next": HOW_ARE_YOU},
 )
